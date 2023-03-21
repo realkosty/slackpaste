1697ad09e4f22db8303c17a51b96ecb1cf6e5386
Chris Stavitsky <corridorr@gmail.com>
2023-03-21 15:21:48 -0700

    instrument sentry

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Sentry
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://1ac0a2b6ddf44c4eb335e506dde7a843@o87286.ingest.sentry.io/4504878909620224",
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
