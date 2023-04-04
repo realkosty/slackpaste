f55e4373dce64a7e6281e47ec654d45cf71afa16
Chris Stavitsky <corridorr@gmail.com>
2023-04-04 14:58:43 -0700

    enable sentry session replay feature

--- src/index.js
+++ src/index.js
@@ -10,8 +10,22 @@ import { BrowserTracing } from "@sentry/tracing";
 
 Sentry.init({
   dsn: "https://1ac0a2b6ddf44c4eb335e506dde7a843@o87286.ingest.sentry.io/4504878909620224",
-  integrations: [new BrowserTracing()],
+  integrations: [
+    new BrowserTracing(),
+    new Sentry.Replay({
+      // Additional SDK configuration goes in here, for example:
+      maskAllText: true,
+      blockAllMedia: true,
+    }),
+  ],
   tracesSampleRate: 1.0,
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  replaysSessionSampleRate: 0.1,
+
+  // If the entire session is not sampled, use the below sample rate to sample
+  // sessions when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
 });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
