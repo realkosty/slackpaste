81a46e1996fdb17bfa356c5270a023476427cc69
Kosty Maleyev <kosty.maleyev@sentry.io>
2024-06-24 09:26:04 -0700

    Parent repo: Auto-formatted + prettier from sentry-javascript (no other changes)

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import './App.css';
 import { processDoc } from './processDoc';
 import { copyToClipboard } from './copyToClipboard';
 import { PopupProvider } from './popupContext';
-import Popup from './popup'
+import Popup from './popup';
 import DeleteButton from './DeleteButton';
-import { usePopup } from './popupContext'
+import { usePopup } from './popupContext';
 
-var emoji = require('node-emoji')
-const SLACK_INPUT = 'slack-input'
-
+var emoji = require('node-emoji');
+const SLACK_INPUT = 'slack-input';
 
 //styling suggestions:
 // create 2 columns:
@@ -17,29 +16,39 @@ const SLACK_INPUT = 'slack-input'
 // and the stripped output on the righthand side
 // this will better follow the slack format
 
-function CopyButton({pasted, disabled}) {
-  const { triggerPopup } = usePopup()
+function CopyButton({ pasted, disabled }) {
+  const { triggerPopup } = usePopup();
 
   const handleCopy = () => {
     if (copyToClipboard()) {
-      triggerPopup(`${emoji.emojify(':white_check_mark:')}  Copied to clipboard!`)
+      triggerPopup(`${emoji.emojify(':white_check_mark:')}  Copied to clipboard!`);
     }
-  }
+  };
 
   return (
-    <p><button className="copy-button" onClick={handleCopy} disabled={disabled}>Copy Formatted Output</button></p>
-  )
+    <p>
+      <button className="copy-button" onClick={handleCopy} disabled={disabled}>
+        Copy Formatted Output
+      </button>
+    </p>
+  );
 }
 
-function Checkbox({label, initialValue, onChangeCallback}) {
+function Checkbox({ label, initialValue, onChangeCallback }) {
   return (
     <div class="option">
       <label>
-        <input type="checkbox" checked={initialValue} onChange={(e) => {onChangeCallback(e.target.checked);}}/>
+        <input
+          type="checkbox"
+          checked={initialValue}
+          onChange={e => {
+            onChangeCallback(e.target.checked);
+          }}
+        />
         {label}
       </label>
     </div>
-  )
+  );
 }
 
 function getFromLocalStorage(key, defaultValue) {
@@ -52,45 +61,50 @@ function App() {
   // to enable us to setDisabled(true) upon clicking the 'clear' button
   // Right now we only setPasted to empty and that leaves the 'copy' button
   // enabled when there is nothing to copy.
-  const [pasted, setPasted] = React.useState('')
-  const [disabled, setDisabled] = React.useState(true)
-  const [originalDoc, setOriginalDoc] = React.useState(null)
-  const [anonymize, setAnonymize] = React.useState(getFromLocalStorage('anonymize', false))
-  const [includeChannelId, setIncludeChannelId] = React.useState(getFromLocalStorage('includeChannelId', true))
-  const [csvExportFriendly, setCsvExportFriendly] = React.useState(getFromLocalStorage('csvExportFriendly', false))
+  const [pasted, setPasted] = React.useState('');
+  const [disabled, setDisabled] = React.useState(true);
+  const [originalDoc, setOriginalDoc] = React.useState(null);
+  const [anonymize, setAnonymize] = React.useState(getFromLocalStorage('anonymize', false));
+  const [includeChannelId, setIncludeChannelId] = React.useState(getFromLocalStorage('includeChannelId', true));
+  const [csvExportFriendly, setCsvExportFriendly] = React.useState(getFromLocalStorage('csvExportFriendly', false));
 
   React.useEffect(() => {
-    document.getElementById(SLACK_INPUT).focus()
-  }, [pasted])
+    document.getElementById(SLACK_INPUT).focus();
+  }, [pasted]);
 
-  const handlePaste = (event) => {
+  const handlePaste = event => {
     let content = event.clipboardData.getData('text/html');
     let newDoc = new DOMParser().parseFromString(content, 'text/html');
     setOriginalDoc(newDoc);
-  }
-  
+  };
+
   React.useEffect(() => {
     if (originalDoc) {
-      let [processed, attachmentsDetected] = processDoc(originalDoc, {anonymize, includeChannelId, csvExportFriendly})
-      let content = new XMLSerializer().serializeToString(processed)
-      setDisabled(false)
-      setPasted(content)
-      if(attachmentsDetected > 0) {
-        const attachmentGrammar = attachmentsDetected > 1 ? 'attachments' : 'attachment'
-        alert(`${attachmentsDetected} ${attachmentGrammar} detected. Remember to download image files and manually include them in notion.`)
+      let [processed, attachmentsDetected] = processDoc(originalDoc, {
+        anonymize,
+        includeChannelId,
+        csvExportFriendly,
+      });
+      let content = new XMLSerializer().serializeToString(processed);
+      setDisabled(false);
+      setPasted(content);
+      if (attachmentsDetected > 0) {
+        const attachmentGrammar = attachmentsDetected > 1 ? 'attachments' : 'attachment';
+        alert(
+          `${attachmentsDetected} ${attachmentGrammar} detected. Remember to download image files and manually include them in notion.`,
+        );
       }
     } else {
       setDisabled(true);
       setPasted('');
     }
-  }, [originalDoc, anonymize, includeChannelId, csvExportFriendly])
+  }, [originalDoc, anonymize, includeChannelId, csvExportFriendly]);
 
   React.useEffect(() => {
     localStorage.setItem('anonymize', JSON.stringify(anonymize));
     localStorage.setItem('includeChannelId', JSON.stringify(includeChannelId));
     localStorage.setItem('csvExportFriendly', JSON.stringify(csvExportFriendly));
-  }, [anonymize, includeChannelId, csvExportFriendly])
-  
+  }, [anonymize, includeChannelId, csvExportFriendly]);
 
   return (
     <PopupProvider>
@@ -99,15 +113,33 @@ function App() {
           <h2 className="header">Paste Slack Thread:</h2>
           <Checkbox label="Anonymize" initialValue={anonymize} onChangeCallback={setAnonymize} />
           <Checkbox label="Include channel ID" initialValue={includeChannelId} onChangeCallback={setIncludeChannelId} />
-          <Checkbox label="CSV export-friendly (link text, code blocks)" initialValue={csvExportFriendly} onChangeCallback={setCsvExportFriendly} />
-          <textarea autoFocus placeholder="paste slack thread here" rows="5" cols="80" onPaste={handlePaste} type="text" id={SLACK_INPUT} autoComplete="no"></textarea>
-          <DeleteButton onClick={() => {document.getElementById(SLACK_INPUT).value = '';  setOriginalDoc(null);}} />
+          <Checkbox
+            label="CSV export-friendly (link text, code blocks)"
+            initialValue={csvExportFriendly}
+            onChangeCallback={setCsvExportFriendly}
+          />
+          <textarea
+            autoFocus
+            placeholder="paste slack thread here"
+            rows="5"
+            cols="80"
+            onPaste={handlePaste}
+            type="text"
+            id={SLACK_INPUT}
+            autoComplete="no"
+          ></textarea>
+          <DeleteButton
+            onClick={() => {
+              document.getElementById(SLACK_INPUT).value = '';
+              setOriginalDoc(null);
+            }}
+          />
           <CopyButton pasted={pasted} disabled={disabled} />
           <Popup />
         </div>
         <div className="col2">
           <div className="output">
-            <div id="formatted-output" dangerouslySetInnerHTML={{__html: pasted}}></div>
+            <div id="formatted-output" dangerouslySetInnerHTML={{ __html: pasted }}></div>
           </div>
         </div>
       </div>
